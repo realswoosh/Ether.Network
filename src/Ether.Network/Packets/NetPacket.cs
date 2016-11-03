f836916ddaf32fdf1c3f8e0bcec73befbcc42e6d
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2016-11-03 14:32:52 +0100

    NetPacket documentation. Remove merged packet

--- src/Ether.Network/Packets/NetPacket.cs
+++ src/Ether.Network/Packets/NetPacket.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Ether.Network.Packets
 {
@@ -15,6 +14,9 @@ namespace Ether.Network.Packets
         private BinaryWriter memoryWriter;
         private PacketStateType state;
 
+        /// <summary>
+        /// Read methods dictionary.
+        /// </summary>
         private static readonly Dictionary<Type, Func<BinaryReader, object>> readMethods = new Dictionary<Type, Func<BinaryReader, object>>()
         {
             { typeof(char), reader => reader.ReadChar()},
@@ -30,6 +32,9 @@ namespace Ether.Network.Packets
             { typeof(string), reader => new string(reader.ReadChars(count: reader.ReadUInt16())) },
         };
 
+        /// <summary>
+        /// Write methods dictionary.
+        /// </summary>
         private static readonly Dictionary<Type, Action<BinaryWriter, object>> writeMethods = new Dictionary<Type, Action<BinaryWriter, object>>()
         {
             { typeof(char), (writer, value) => writer.Write((char)value) },
@@ -61,8 +66,6 @@ namespace Ether.Network.Packets
             {
                 if (this.state == PacketStateType.Write)
                 {
-                    this.Size = Convert.ToInt32(this.memoryStream.Length);
-
                     this.memoryWriter.Seek(0, SeekOrigin.Begin);
                     this.Write(this.Size);
                     this.memoryWriter.Seek(this.Size, SeekOrigin.Begin);
@@ -76,8 +79,26 @@ namespace Ether.Network.Packets
             }
         }
 
-        public int Size { get; internal set; }
+        /// <summary>
+        /// Gets the size of the packet.
+        /// </summary>
+        public int Size
+        {
+            get { return (int)this.memoryStream.Length; }
+        }
 
+        /// <summary>
+        /// Gets or sets the read/write position in the packet.
+        /// </summary>
+        public long Position
+        {
+            get { return (int)this.memoryStream.Position; }
+            set { this.memoryStream.Position = value; }
+        }
+
+        /// <summary>
+        /// Creates a new NetPacket in write-only mode.
+        /// </summary>
         public NetPacket()
         {
             this.state = PacketStateType.Write;
@@ -87,6 +108,10 @@ namespace Ether.Network.Packets
             this.Write(0); // packet size
         }
 
+        /// <summary>
+        /// Creates a new NetPacket in read-only mode.
+        /// </summary>
+        /// <param name="buffer"></param>
         public NetPacket(byte[] buffer)
         {
             this.state = PacketStateType.Read;
@@ -95,6 +120,11 @@ namespace Ether.Network.Packets
             this.memoryReader = new BinaryReader(this.memoryStream);
         }
 
+        /// <summary>
+        /// Writes a T value in the packet.
+        /// </summary>
+        /// <typeparam name="T">Value type.</typeparam>
+        /// <param name="value">Value.</param>
         public void Write<T>(T value)
         {
             if (this.state != PacketStateType.Write)
@@ -106,6 +136,11 @@ namespace Ether.Network.Packets
                 writeMethods[type](this.memoryWriter, value);
         }
 
+        /// <summary>
+        /// Reads a T value from the packet.
+        /// </summary>
+        /// <typeparam name="T">Value type.</typeparam>
+        /// <returns>Value.</returns>
         public T Read<T>()
         {
             if (this.state != PacketStateType.Read)
@@ -119,6 +154,9 @@ namespace Ether.Network.Packets
             return default(T);
         }
 
+        /// <summary>
+        /// Dispose the NetPacket resources.
+        /// </summary>
         public void Dispose()
         {
             if (this.memoryReader != null)
@@ -140,6 +178,11 @@ namespace Ether.Network.Packets
             }
         }
 
+        /// <summary>
+        /// Split the incoming packets.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
         public static NetPacket[] Split(byte[] buffer)
         {
             ICollection<NetPacket> packets = new List<NetPacket>();
@@ -155,11 +198,8 @@ namespace Ether.Network.Packets
 
                         if (packetSize == 0)
                             break;
-
-                        var newPacket = new NetPacket(readerStream.ReadBytes(packetSize));
-                        newPacket.Size = packetSize;
-
-                        packets.Add(newPacket);
+                        
+                        packets.Add(new NetPacket(readerStream.ReadBytes(packetSize)));
                     }
                 }
                 catch (Exception e)
