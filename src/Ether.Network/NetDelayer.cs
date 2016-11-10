aa39f407293be62f239186bb9b9e24b4314a9433
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2016-11-10 14:11:46 +0100

    Add NetDelayer

--- src/Ether.Network/NetDelayer.cs
+++ src/Ether.Network/NetDelayer.cs
@@ -1,34 +1,106 @@
-﻿using System;
+﻿using Ether.Network.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Ether.Network
 {
-    /// <summary>
-    /// [WIP] Delayer.
-    /// </summary>
     public static class NetDelayer
     {
-        private static List<NetDelayerAction> actions = new List<NetDelayerAction>();
+        private static object syncActionsRoot = new object();
+        private static ICollection<NetDelayerAction> actions;
 
+        /// <summary>
+        /// Gets the running state of the NetDelayer.
+        /// </summary>
         public static bool IsRunning { get; internal set; }
 
-
+        /// <summary>
+        /// Initialize the static properties of the NetDelayer.
+        /// </summary>
         static NetDelayer()
         {
             IsRunning = false;
+            actions = new List<NetDelayerAction>();
         }
 
-        public static void Register(string actionName, Action action, uint milliseconds)
+        /// <summary>
+        /// Register an action starting now with no recursion.
+        /// </summary>
+        /// <param name="action">Action to execute</param>
+        /// <returns>Action Id</returns>
+        public static int Register(Action action)
         {
+            return Register(action, DateTime.Now, 0, false);
         }
 
-        public static void Unregister(string actionName)
+        /// <summary>
+        /// Register an action starting at the specified time.
+        /// </summary>
+        /// <param name="action">Action to execute</param>
+        /// <param name="callTime">Time to execute the action</param>
+        /// <returns>Action Id</returns>
+        public static int Register(Action action, DateTime callTime)
         {
+            return Register(action, callTime, 0, false);
         }
 
+        /// <summary>
+        /// Register an action starting at the speficied time.
+        /// This action is recursive every specified milliseconds.
+        /// </summary>
+        /// <param name="action">Action to execute</param>
+        /// <param name="callTime">Time to execute the action</param>
+        /// <param name="recursionTime">Recursion time in milliseconds</param>
+        /// <returns>Action Id</returns>
+        public static int Register(Action action, DateTime callTime, int recursionTime)
+        {
+            return Register(action, callTime, recursionTime, true);
+        }
+
+        /// <summary>
+        /// Register an action.
+        /// </summary>
+        /// <param name="action">Action to execute</param>
+        /// <param name="callTime">Time to execute the action</param>
+        /// <param name="recursionTime">Recursion time in milliseconds</param>
+        /// <param name="isRecursive">Is action recursive</param>
+        /// <returns>Action Id</returns>
+        private static int Register(Action action, DateTime callTime, int recursionTime, bool isRecursive)
+        {
+            var delayedAction = new NetDelayerAction(
+                action,
+                callTime,
+                recursionTime,
+                isRecursive);
+
+            lock (syncActionsRoot)
+            {
+                actions.Add(delayedAction);
+            }
+
+            return delayedAction.Id;
+        }
+
+        /// <summary>
+        /// Unregister an action.
+        /// </summary>
+        /// <param name="actionId">Action Id</param>
+        public static void Unregister(int actionId)
+        {
+            lock (syncActionsRoot)
+            {
+                var action = actions.FirstOrDefault(x => x.Id == actionId);
+
+                if (action != null)
+                    actions.Remove(action);
+            }
+        }
+
+        /// <summary>
+        /// Starts the NetDelayer.
+        /// </summary>
         public static void Start()
         {
             if (IsRunning)
@@ -38,11 +110,31 @@ namespace Ether.Network
 
             while (IsRunning)
             {
+                IEnumerable<NetDelayerAction> actionsReady = new List<NetDelayerAction>();
+
+                lock (syncActionsRoot)
+                {
+                    actionsReady = from x in actions
+                                   where x.CallTime < DateTime.Now
+                                   select x;
+                }
+
+                foreach (var action in actionsReady)
+                {
+                    action.Action();
+                    if (action.IsRecursive)
+                        action.CallTime = DateTime.Now.AddMilliseconds(action.RecursionTime);
+                    else
+                        Unregister(action.Id);
+                }
 
                 Thread.Sleep(1);
-            }   
+            }
         }
 
+        /// <summary>
+        /// Stop the NetDelayer.
+        /// </summary>
         public static void Stop()
         {
             IsRunning = false;
@@ -51,5 +143,75 @@ namespace Ether.Network
 
     internal class NetDelayerAction
     {
+        /// <summary>
+        /// Gets or sets the NetDelayerAction Id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action to execute.
+        /// </summary>
+        public Action Action { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time that the action will be executed.
+        /// </summary>
+        public DateTime CallTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the recursion time in milliseconds.
+        /// </summary>
+        public int RecursionTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets if the action is recursive or not.
+        /// </summary>
+        public bool IsRecursive { get; set; }
+
+        /// <summary>
+        /// Creates a new NetDelayerAction instance.
+        /// </summary>
+        /// <param name="action"></param>
+        public NetDelayerAction(Action action)
+            : this(action, DateTime.Now, 0, false)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new NetDelayerAction instance.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="callTime"></param>
+        public NetDelayerAction(Action action, DateTime callTime)
+            : this(action, callTime, 0, false)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new NetDelayerAction instance.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="callTime"></param>
+        /// <param name="recursionTime"></param>
+        public NetDelayerAction(Action action, DateTime callTime, int recursionTime)
+            : this(action, callTime, recursionTime, true)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new NetDelayerAction instance.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="callTime"></param>
+        /// <param name="recursionTime"></param>
+        /// <param name="isRecursive"></param>
+        public NetDelayerAction(Action action, DateTime callTime, int recursionTime, bool isRecursive)
+        {
+            this.Id = Helper.GenerateUniqueId();
+            this.Action = action;
+            this.CallTime = callTime;
+            this.RecursionTime = recursionTime;
+            this.IsRecursive = isRecursive;
+        }
     }
 }
---
+++ test/Ether.Network.Tests/NetDelayerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Ether.Network;
+
+namespace Ether.Network.Tests
+{
+    public class NetDelayerTest
+    {
+        [Fact]
+        public void NetDelayerRegisterSuccess()
+        {
+        }
+
+        [Fact]
+        public void NetDelayerRegisterFailed()
+        {
+        }
+    }
+}
